f89ba5e3c0c275fce4afa04cfc94ff8d040f2f88
oniisan-rakki <153196954+oniisan-rakki@users.noreply.github.com>
2024-08-05 11:19:48 +0200

    Update mixed-messages.js

--- mixed-messages.js
+++ mixed-messages.js
@@ -1,1 +1,13 @@
-// Topic Questions to Ask the Interviewer
+// Random Number Generator
+function randomNumber() {
+    let randNum = Math.floor(Math.random() * 3);
+    return randNum;
+}
+
+// Lists
+const datasetOne = ['Who', 'What', 'Where'];
+const datasetTwo = ['left', 'came', 'is'];
+const datasetThree = ['work','up', 'Thabang'];
+
+console.log(`${datasetOne[randomNumber()]} ${datasetTwo[randomNumber()]} ${datasetThree[randomNumber()]}?`)
+
